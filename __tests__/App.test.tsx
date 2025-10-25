3e782bdea8bcafb157e498900921e1bb7f5596aa
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-25 20:21:30 +0530

    Add testcase to check initial render

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,9 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
+import { render ,screen } from "@testing-library/react-native";
 
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
-  });
+ render(<App />)
+  expect(screen.getByText("Everest Canteen")).toBeTruthy();
+    
 });
