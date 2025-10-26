2f085dd175620909b59a0498ea12ac0e51a9ce5e
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-27 00:13:30 +0530

    Add testcase to check header render

--- __tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../App';
-import { render ,screen } from "@testing-library/react-native";
-
-test('renders correctly', async () => {
- render(<App />)
-  expect(screen.getByText("Everest Canteen")).toBeTruthy();
-    
-});
