6f3dca1362af42897280526b7e50a33816ee6e57
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-27 15:25:01 +0530

    Add test case for handle form data

--- __tests__/Modal.test.tsx
+++ __tests__/Modal.test.tsx
@@ -1,10 +1,23 @@
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen} from "@testing-library/react-native";
 import Modal from "../src/components/Modal";
+import { Alert } from "react-native";
 
 describe("Modal test cases" , ()=>{
+    jest.spyOn(Alert , "alert")
     test("it should render the model" , ()=>{
-        render(<Modal sectionName={""} onClose={function (): void {} } 
+        render(<Modal sectionName={"Beverages"} onClose={function (): void {} } 
         addItem={function (_name: string, _price: number): void {} } />)
-        expect(screen.getByText(/Item Name/i)).toBeTruthy();
+        expect(screen.getByText(/Beverages/i)).toBeTruthy();
+    })
+    test("is should validate inputs" , ()=>{
+        render(<Modal sectionName={"Break fast"} onClose={function (): void {} }
+        addItem={function (_name: string, _price: number): void {} } />)
+        const inputElement1 = screen.getByPlaceholderText("Enter the item name")
+        const addBtn = screen.getByText("Add")
+        fireEvent.changeText(inputElement1 , "Tea")
+        fireEvent.press(addBtn)
+        expect(Alert.alert).toHaveBeenCalledWith("fill all fields") 
     })
 })
+
+
