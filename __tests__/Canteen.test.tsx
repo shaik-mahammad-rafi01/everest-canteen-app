652d5562d2a50905c0d639a2266caceb96e54fdf
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-27 15:47:02 +0530

    Add testcase to open modal when click on add button

--- __tests__/Canteen.test.tsx
+++ __tests__/Canteen.test.tsx
@@ -1,9 +1,17 @@
-import { render, screen } from "@testing-library/react-native";
+import { render, screen, userEvent} from "@testing-library/react-native";
 import Canteen from "../src/components/Canteen";
 
 describe("Canteen test cases" , ()=>{
     test("it should render canteen menu items" , ()=>{
         render(<Canteen />)
         expect(screen.getByText("Beverages")).toBeTruthy();
+    })
+    test("it should open modal properly" , async()=>{
+        render(<Canteen />)
+        const user = userEvent.setup()
+        const addBtns =await screen.getAllByText("Add");
+        await user.press(addBtns[0])
+        const text = screen.getByText(/Cancel/i)
+        expect(text).toBeTruthy();
     })
 })
