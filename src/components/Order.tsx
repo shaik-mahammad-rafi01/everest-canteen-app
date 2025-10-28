ce8170deab30c04efb76bc57d5853c0bba87b6bd
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-28 15:55:45 +0530

    Add order component

---
+++ src/components/Order.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View , Text} from 'react-native';
+
+const Order = () => {
+  return (
+    
+    <View>
+        <Text>Order</Text>
+    </View>
+  );
+}
+
+export default Order;
