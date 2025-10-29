83d470b51e07aa526bbe06a50291d29b5a7ebe27
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-30 00:27:11 +0530

    Modify screen names and refactor test cases

--- src/navigation/TabNavigation.test.tsx
+++ src/navigation/TabNavigation.test.tsx
@@ -4,17 +4,17 @@ import { TabNavigations } from './TabNavigation';
 describe('TabNavigation component testcases', () => {
   test('it should render the tab navigation properly', () => {
     render(<TabNavigations />);
-    const tab = screen.getByText(/Order/i);
+    const tab = screen.getByText("Order");
     fireEvent.press(tab);
-    expect(screen.getByText).toBeTruthy();
+    expect(screen.getByText("No Orders yet")).toBeTruthy();
   });
-  test('it should switch screen from order to menu', () => {
+  test('it should switch screen from order to canteen menu', () => {
     render(<TabNavigations />);
-    const tab = screen.getByText(/Order/i);
+    const tab = screen.getByText("Order");
     fireEvent.press(tab);
-    expect(screen.getByText).toBeTruthy();
+    expect(screen.getByText("No Orders yet")).toBeTruthy();
 
-    const menu = screen.getByText(/Menu/i);
+    const menu = screen.getByText("Canteen Menu");
     fireEvent.press(menu);
     expect(screen.getByText(/Everest Canteen/i)).toBeTruthy();
   });
--- src/navigation/tabNavigation.tsx
+++ src/navigation/tabNavigation.tsx
@@ -1,18 +1,18 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Canteen from '../components/Canteen/Canteen';
 import Order from '../components/Order/Order';
 import { NavigationContainer } from '@react-navigation/native';
+import CanteenMenu from '../components/CanteenMenu/CanteenMenu';
 const Tab = createBottomTabNavigator();
 export const TabNavigations = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="Menu"
+        initialRouteName="Canteen Menu"
         screenOptions={{ animation: 'shift' }}
       >
         <Tab.Screen
-          name="Menu"
-          component={Canteen}
+          name="Canteen Menu"
+          component={CanteenMenu}
           options={{ headerShown: false }}
         />
         <Tab.Screen name="Order" component={Order} />
