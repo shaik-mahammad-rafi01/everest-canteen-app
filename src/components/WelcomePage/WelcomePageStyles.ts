005c82f8bb4f9252a3171ef79144887d46b3734e
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-30 17:32:38 +0530

    Add welcomePage styles file

---
+++ src/components/WelcomePage/WelcomePageStyles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const WelcomeStyles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: '#FCBA03',
+    },
+    titleContainer: {
+        marginTop: 40,
+    },
+    welcomeHeader: {
+        fontSize: 20,
+        fontWeight: '700'
+    },
+    buttonsContainer: {
+        gap: 15,
+        alignItems: 'center',
+    },
+    footerContainer: {
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#fff',
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 8,
+        cursor: 'pointer',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+    },
+});
