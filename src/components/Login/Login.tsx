119ba1d8741d47b2ff64dd6e36b14c0a65760528
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-30 22:41:13 +0530

    Add Login screen component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { LoginStyles } from './LoginStyles';
+
+const Login = () => {
+  return (
+    <View style={LoginStyles.container}>
+      <View style={LoginStyles.headerContainer}>
+        <Text style={LoginStyles.headerText}>Sign in your account</Text>
+      </View>
+
+      <View style={LoginStyles.formContainer}>
+        <Text style={LoginStyles.label}>Username</Text>
+        <TextInput
+          style={LoginStyles.input}
+          placeholder="Enter your name"
+          placeholderTextColor="#aaa"
+        />
+
+        <Text style={LoginStyles.label}>Password</Text>
+        <TextInput
+          style={LoginStyles.input}
+          placeholder="Enter your password"
+          placeholderTextColor="#aaa"
+          secureTextEntry
+        />
+
+        <Pressable style={LoginStyles.signInButton}>
+          <Text style={LoginStyles.signInText}>SIGN IN</Text>
+        </Pressable>
+
+        <Text style={LoginStyles.orText}>or sign in with</Text>
+      </View>
+
+      <View style={LoginStyles.footerContainer}>
+        <Text style={LoginStyles.footerText}>Don't have an account?</Text>
+        <Pressable>
+          <Text style={LoginStyles.signUpText}>SIGN UP</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
