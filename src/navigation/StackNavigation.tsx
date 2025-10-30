1995d4550eeffed85871549fc7ef666acd47c10b
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-31 00:21:35 +0530

    Add stack-navigation component to switch between welcome to login

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+import WelcomePage from '../components/WelcomePage/WelcomePage';
+import Login from '../components/Login/Login';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const StackNavigaton = () => {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="home" component={WelcomePage} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigaton;
