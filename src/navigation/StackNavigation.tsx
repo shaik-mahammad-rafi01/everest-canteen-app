251e3f037091fd33eadf4dcdd38621aa32e2e891
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-31 01:41:43 +0530

    Add Register page in stack-navigation

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import WelcomePage from '../components/WelcomePage/WelcomePage';
 import Login from '../components/Login/Login';
 import { createStackNavigator } from '@react-navigation/stack';
+import Register from '../components/Register/Register';
 
 const StackNavigaton = () => {
   const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ const StackNavigaton = () => {
       <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="home" component={WelcomePage} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
