8a17ab905fa72f013835c89e62ffe6a5bbb28c2c
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-02 02:51:57 +0530

    Add testcase to check it redirect to user login

--- src/components/WelcomePage/WelcomePage.test.tsx
+++ src/components/WelcomePage/WelcomePage.test.tsx
@@ -1,10 +1,18 @@
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import WelcomePage from './WelcomePage';
 
 describe('WelcomePage test cases', () => {
+  const route = { params: { role: 'User' } };
+  const navigation = {navigate : jest.fn()}
   test('it should render the component properly', () => {
     render(<WelcomePage />);
     const text = screen.getByText('A CANTEEN JUST FOR YOU');
     expect(text).toBeTruthy();
   });
+  test("it should redirect login after clicking Login as user" , ()=>{
+    render(<WelcomePage route={route} navigation={navigation}/>)
+    const button  = screen.getByText("LOGIN AS USER");
+    fireEvent.press(button);
+    expect(navigation.navigate).toHaveBeenCalledWith("Login" , {role : "User"});
+  })
 });
