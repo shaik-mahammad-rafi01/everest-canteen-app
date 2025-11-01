b300779d68af3b536c8813e7787dbf638997526a
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-02 00:14:07 +0530

    Add testcase to get alert when click sign-in button with empty fields

--- src/components/Login/Login.test.tsx
+++ src/components/Login/Login.test.tsx
@@ -1,9 +1,11 @@
-import { render , screen} from "@testing-library/react-native";
+import { fireEvent, render , screen} from "@testing-library/react-native";
 import Login from "./Login";
+import { Alert } from "react-native";
 
 describe("Login page test cases" , ()=>{
   const mockNavigate = jest.fn();
   const mockRoute = (role = 'Admin') => ({ params: { role } });
+  const mockAlert = jest.spyOn(Alert, 'alert');
 
   test('it should render login screen with role text', () => {
     render(<Login route={mockRoute('User')} navigation={{ navigate: mockNavigate }} />);
@@ -13,5 +15,12 @@ describe("Login page test cases" , ()=>{
   test("it should render the admin page" , ()=>{
         render(<Login route={mockRoute('Admin')} navigation={{ navigate: mockNavigate }} />);
     expect(screen.getByText('Sign in as Admin')).toBeTruthy();
+  })
+
+  test("it should alert when click signIn with empty fields" , ()=>{
+     render(<Login route={mockRoute('Admin')} navigation={{ navigate: mockNavigate }} />);
+    const button = screen.getByText("SIGN IN")
+    fireEvent.press(button)
+    expect(mockAlert).toHaveBeenCalledWith("Please fill username and password")
   })
 })
