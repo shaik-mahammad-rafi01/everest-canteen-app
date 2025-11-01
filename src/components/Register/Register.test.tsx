6218ad7d68aeec4c76019dac9b2fa7c769d110a6
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-02 01:17:18 +0530

    Add testcase for Register page alert on empty input fields

--- src/components/Register/Register.test.tsx
+++ src/components/Register/Register.test.tsx
@@ -1,10 +1,21 @@
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import Register from './Register';
-
-test('renders Register', () => {
+import { Alert } from 'react-native';
+describe('Register page test cases', () => {
   const route = { params: { role: 'User' } };
   const navigation = jest.fn();
+  const mockAlert = jest.spyOn(Alert, 'alert');
+  test('renders Register', () => {
+    render(<Register route={route} navigation={navigation} />);
+    expect(screen.getByText('Sign Up as User')).toBeTruthy();
+  });
 
-  render(<Register route={route} navigation={navigation} />);
-  expect(screen.getByText('Sign Up as User')).toBeTruthy();
+  test('It should alert when input fields are empty', () => {
+    render(<Register route={'Admin'} navigation={navigation}/>)
+        fireEvent.changeText(screen.getByPlaceholderText('Enter your name'), '');
+        fireEvent.changeText(screen.getByPlaceholderText('Enter your password'), '');
+        fireEvent.changeText(screen.getByPlaceholderText('Confirm your password'), '');
+        fireEvent.press(screen.getByText("SIGN UP"));
+        expect(mockAlert).toHaveBeenCalledWith("Please fill all fields")
+  });
 });
