1ec47ecb7ec83613589ccca2c307cb0cf4ac99b3
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-02 01:44:24 +0530

    Add testcase to check it redirect to login after successful registration

--- src/components/Register/Register.test.tsx
+++ src/components/Register/Register.test.tsx
@@ -3,7 +3,7 @@ import Register from './Register';
 import { Alert } from 'react-native';
 describe('Register page test cases', () => {
   const route = { params: { role: 'User' } };
-  const navigation = jest.fn();
+  const navigation = {navigate : jest.fn()}
   const mockAlert = jest.spyOn(Alert, 'alert');
   test('renders Register', () => {
     render(<Register route={route} navigation={navigation} />);
@@ -11,11 +11,20 @@ describe('Register page test cases', () => {
   });
 
   test('It should alert when input fields are empty', () => {
-    render(<Register route={'Admin'} navigation={navigation}/>)
+    render(<Register route={'User'} navigation={navigation}/>)
         fireEvent.changeText(screen.getByPlaceholderText('Enter your name'), '');
         fireEvent.changeText(screen.getByPlaceholderText('Enter your password'), '');
         fireEvent.changeText(screen.getByPlaceholderText('Confirm your password'), '');
         fireEvent.press(screen.getByText("SIGN UP"));
         expect(mockAlert).toHaveBeenCalledWith("Please fill all fields")
+  });
+    test('It should redict to Login after register', () => {
+    render(<Register route={route} navigation={navigation}/>)
+        fireEvent.changeText(screen.getByPlaceholderText('Enter your name'), 'rafi');
+        fireEvent.changeText(screen.getByPlaceholderText('Enter your password'), '1234');
+        fireEvent.changeText(screen.getByPlaceholderText('Confirm your password'), '1234');
+        fireEvent.press(screen.getByText("SIGN UP"));
+        expect(navigation.navigate).toHaveBeenCalledWith("Login" , {role : "User"})
+        expect(screen.getByText("SIGN IN")).toBeTruthy();
   });
 });
