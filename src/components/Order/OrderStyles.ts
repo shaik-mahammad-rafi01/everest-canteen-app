a5108185c4e7cf8fb45a1cf1728617221e1a4e3b
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-04 11:23:01 +0530

    Add order component styles

---
+++ src/components/Order/OrderStyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+export const OrderStyles = StyleSheet.create({
+  itemContainer: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  name:{
+    color:'red',
+    backgroundColor:'orange'
+  }
+});
