2c8c809b2825ef98faa6662b69894f9ce885a2f5
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-04 10:32:45 +0530

    Implement getOrders fucntion to fetch orders from firebase to display

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import { MenuItem } from '../../types/menu';
+import { OrderStyles } from './OrderStyles';
 
 const Order = () => {
+  const [orders, setOrders] = useState<MenuItem[]>([]);
+  
+  const getOrders = async ()=> {
+    const response = await fetch('http://localhost:3000/getOrder');
+    const allOrders = await (response.json());
+    setOrders(allOrders);
+    console.log(allOrders)
+   
+  };
+  useEffect(() => {
+    getOrders();
+    
+  },[]);
+
   return (
     <View>
-      <Text>Order</Text>
+      <View>
+        <Text>Order</Text>
+      </View>
+      <View>
+        <FlatList
+          data={orders}
+         
+          renderItem={({ item }) => (
+            <View style={OrderStyles.itemContainer}>
+              <Text style = {OrderStyles.name}>{item.name}</Text>
+              <Text>{item.price}</Text>
+              <Image style={OrderStyles.image} source={{ uri: item.image }} />
+            </View>
+            
+          )}
+        />
+        
+      </View>
     </View>
   );
 };
