1f18608dbda3de6f982106dac74c3b69345e8d53
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-04 22:47:50 +0530

    Add styles to Button component

---
+++ src/components/Button/ButtonStyles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const ButtonStyles = StyleSheet.create({
+      button: {
+        backgroundColor: '#fff',
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 8,
+        cursor: 'pointer',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+    },
+})
