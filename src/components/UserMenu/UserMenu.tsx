bcafc4bd0394609e9fd276a18931f7f16669c3e7
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-09 20:58:14 +0530

    Add logout button in userMenu for safely logout from the screen

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-import { View, SectionList, Text } from 'react-native';
+import { View, SectionList, Text, Pressable } from 'react-native';
 import { CanteenMenuStyles } from '../CanteenMenu/CanteenMenuStyles';
 import CardItem from '../ItemCard/ItemCard';
 import { CanteenItems } from '../../data/menu';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { UserStyles } from './UserStyles';
 
-const UserMenu = () => {
+const UserMenu = ({ navigation }: any) => {
   const [canteenItems] = useState(CanteenItems);
 
   return (
     <SafeAreaView>
-      <View>
-        <Text style={UserStyles.Header}>Our Menu</Text>
+      <View >
+        <View style={UserStyles.container}>
+        <View>
+          <Text style={UserStyles.Header}>Our Menu</Text>
+        </View>
+        <View>
+          <Pressable onPress={() => navigation.navigate('home')} style={UserStyles.logoutBtn2}>
+            <Text>Logout</Text>
+          </Pressable>
+        </View>
+        </View>
+
         <SectionList
           sections={canteenItems}
           keyExtractor={(item, index) => item.name + index}
-          renderItem={({ item }) => <CardItem item={item} Delete={() => ''} role={'User'} />}
+          renderItem={({ item }) => (
+            <CardItem item={item} Delete={() => ''} role={'User'} />
+          )}
           renderSectionHeader={({ section: { FoodType, data } }) => (
             <View style={CanteenMenuStyles.header}>
               <Text>{FoodType}</Text>
--- src/components/UserMenu/UserStyles.ts
+++ src/components/UserMenu/UserStyles.ts
@@ -1,8 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const UserStyles = StyleSheet.create({
-    Header:{
-        fontSize:20,
-        fontWeight:'700'
+    Header: {
+        fontSize: 20,
+        fontWeight: '700'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-around",
+        alignItems: 'center'
+    },
+    logoutBtn2: {
+        borderWidth: 1,
+        padding: 3,
+        borderRadius: 5,
+        marginBottom:2
     }
+
 })
