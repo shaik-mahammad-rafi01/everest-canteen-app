9cc7abc311c352072ea20c7783a8515ce01c3161
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-11 23:28:41 +0530

    Add ProfileImage component

---
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Image, Platform, View } from 'react-native';
+import { Pressable } from 'react-native-gesture-handler';
+import { ProfileImageStyles } from './ProfileImageStyles';
+import { PERMISSIONS, request, RESULTS } from 'react-native-permissions';
+import {
+  ImageLibraryOptions,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+
+const ProfileImage = () => {
+  const [image, setImage] = useState<string | undefined>();
+
+  const handleProfileImage = async () => {
+    const permission =
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.PHOTO_LIBRARY
+        : PERMISSIONS.ANDROID.READ_MEDIA_IMAGES;
+
+    try {
+      const result = await request(permission);
+
+      if (result === RESULTS.GRANTED) {
+        console.log('Access granted');
+
+        const options: ImageLibraryOptions = {
+          mediaType: 'photo',
+          quality: 1,
+        };
+        const response = await launchImageLibrary(options);
+
+        if (!response.didCancel && response.assets) {
+          const myNewImg = response.assets[0].uri;
+          setImage(myNewImg);
+        }
+      } else if (result === RESULTS.DENIED) {
+        console.log('Permission denied');
+      }
+    } catch (error) {
+      console.error('Error requesting permission:', error);
+    }
+  };
+  return (
+    <View>
+      <Pressable onPress={handleProfileImage}>
+        <Image
+          style={ProfileImageStyles.profileImage}
+          source={{
+            uri:
+              image ||
+              'https://img.icons8.com/?size=100&id=44614&format=png&color=000000',
+          }}
+        />
+      </Pressable>
+    </View>
+  );
+};
+
+export default ProfileImage;
