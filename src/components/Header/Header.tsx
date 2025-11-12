467bddbeec107e4580a5ab41d23a126a8df42001
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-12 16:34:18 +0530

    Render ProfileImage component in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,13 @@ import { Image, View } from 'react-native';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { HeaderStyles } from './HeaderStyles';
+import {ProfileImage} from '../ProfileImage/ProfileImage'
 
 const Header = () => {
   return (
     <SafeAreaView>
       <View style={HeaderStyles.headerContainer}>
+          <ProfileImage />
         <View>
           <Image style={HeaderStyles.icon} source={require('../asserts/icon.png')} />
         </View>
