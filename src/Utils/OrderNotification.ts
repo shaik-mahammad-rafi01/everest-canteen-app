1cfc335574e7803aaa74a5d684bab107a664389e
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-14 00:52:34 +0530

    Update the handleOrderNotification function to works in android

--- src/Utils/OrderNotification.ts
+++ src/Utils/OrderNotification.ts
@@ -4,17 +4,32 @@ import { getApplicationName } from 'react-native-device-info';
 
 export const handleOrderNotification = async () => {
 
-    await notifee.requestPermission();
+
     const MyAppName = Platform.OS === 'ios' ? await getApplicationName() : "Everest Canteen";
+    if (Platform.OS === "ios") {
+        await notifee.requestPermission();
+    }
+    else if (Platform.OS === "android") {
+        await notifee.requestPermission();
+    }
+    const channelId = await notifee.createChannel({
+        id: "Hey_Rafi",
+        name: "Order got placed !",
+        sound: 'default',
 
-
+    })
     await notifee.displayNotification({
         title: `${MyAppName}`,
         subtitle: "Hello Rafi ",
         body: 'Your order got placed!',
+        android: {
+            channelId,
+            sound:'default'
+        },
         ios: {
             sound: 'default'
         },
+
     });
 
 };
