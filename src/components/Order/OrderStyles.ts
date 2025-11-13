40ddebe5657289fe85b6bb5907b2c89d085bf8b2
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-13 19:33:08 +0530

    Add Order component styles

---
+++ src/components/Order/OrderStyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const OrderStyles = StyleSheet.create({
+    container:{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center'
+    },
+  orderSubmitButtonContainer: {
+    position: 'absolute',
+    marginTop:650,
+    textAlign:'center',
+    alignContent:'center'
+  },
+
+})
